24442072c218dd3c04d53edfbbce2cc2f159a0a8
DMisztal <dominikmisztal@onet.eu>
2023-03-06 17:03:19 +0100

    Refined the solution to the problem

--- lab2/main.py
+++ lab2/main.py
@@ -19,7 +19,7 @@ def get_num(string_nums):
         string_len += power * int(string_nums[x])
         power = power/10
         x+=1
-    return string_len
+    return string_len, count
     
 
 def my_printf(format_string,param):
@@ -28,9 +28,10 @@ def my_printf(format_string,param):
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == '.':
-                str_len = get_num(format_string[idx+2:])
+                str_len, count = get_num(format_string[idx+2:])
                 print(param[:str_len].swapcase(),end="")
                 shouldDo=False
+                print(format_string[idx + 3 + count:],end="")
             else:
                 print(format_string[idx],end="")
         else:
