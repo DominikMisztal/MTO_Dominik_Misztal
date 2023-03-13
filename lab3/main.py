bf3b9b74de1350becb9fab22ee4bb3f7bd17c797
DMisztal <dominikmisztal@onet.eu>
2023-03-13 16:42:32 +0100

    Implementation of lab2 using regex

--- lab3/main.py
+++ lab3/main.py
@@ -16,8 +16,17 @@ def my_printf(format_string,param):
                 max = result.group(3)
                 if not result:
                     break;
+                if min and min[0] == '0':
+                    break;
+                if (dot and max and max[0] == '0') or (not dot and max):
+                    break;
+                
                 if not min and not dot and not max:
                     print(param, end='')
+                elif not min and dot and max:
+                    maxInt = int(max)
+                    print(f'{param:{maxInt}}', end='')
+                
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
