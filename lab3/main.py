592998163cc07a4aa34d00cb4ebcf2d603d7307a
DMisztal <dominikmisztal@onet.eu>
2023-03-13 16:49:12 +0100

    added fixes to improve compatibility with lab2 tests, all tests finished

--- lab3/main.py
+++ lab3/main.py
@@ -18,20 +18,23 @@ def my_printf(format_string,param):
                     break;
                 if min and min[0] == '0':
                     break;
-                if (dot and max and max[0] == '0') or (not dot and max):
+                if not dot and max:
                     break;
                 
                 if not min and not dot and not max:
                     print(param, end='')
                 elif not min and dot and max:
                     maxInt = int(max)
-                    print(f'{param:{maxInt}}', end='')
+                    if maxInt  != 0 and max[0] == '0':
+                        break
+                    print(f'{param:.{maxInt}}', end='')
                 
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
         else:
-            shouldDo=True
+            if(format_string[idx] == 'k'):
+                shouldDo=True
     print("")
 
 data=sys.stdin.readlines()
