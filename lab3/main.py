9f8d6b12019b747001ea425906873033aeb3cdc5
DMisztal <dominikmisztal@onet.eu>
2023-03-13 16:28:56 +0100

    implemented lab1 functionality

--- lab3/main.py
+++ lab3/main.py
@@ -3,8 +3,8 @@
 import sys
 
 def my_printf(format_string,param):
-    #print(format_string)
     shouldDo=True
+    param = param.swapcase()
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'k':
