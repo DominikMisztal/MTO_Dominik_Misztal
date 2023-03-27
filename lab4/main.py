8bbc583af34ba58a4a7f80107188892dbb5a3f2c
DMisztal <dominikmisztal@onet.eu>
2023-03-27 16:52:17 +0200

    Casting numbers to int

--- lab4/main.py
+++ lab4/main.py
@@ -4,11 +4,15 @@ import sys
 
 def my_printf(format_string,param):
     #print(format_string)
+    if(not param.isdigit()):
+        exit()
     shouldDo=True
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'g':
-                print(param[::-1],end="")
+                num = str(int(param))
+                num = str(int(num[::-1]))
+                print(num,end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
