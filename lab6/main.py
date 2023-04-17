08bda761c26afa36587d065916bc6793c9601ef4
DMisztal <dominikmisztal@onet.eu>
2023-04-17 16:23:03 +0200

    Searching for regex in string

--- lab6/main.py
+++ lab6/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 def my_printf(format_string,param):
     shouldDo=True
@@ -9,8 +10,12 @@ def my_printf(format_string,param):
 
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+            if format_string[idx] == '#' and done == False:
+                result = re.search(regex, format_string[idx:])
+                if not result:
+                    break
+                
+                done = True
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
