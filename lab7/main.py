759304553f0b0ee30f3e8627a4193d8879954aff
DMisztal <dominikmisztal@onet.eu>
2023-04-24 16:19:48 +0200

    New argument format

--- lab7/main.py
+++ lab7/main.py
@@ -3,11 +3,11 @@
 import sys
 
 def my_printf(format_string,param):
-    #print(format_string)
     shouldDo=True
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
+            if format_string[idx] == '#' and format_string[idx+1] == 'j':
+                
                 print(param,end="")
                 shouldDo=False
             else:
